3079f8b3d524c3714ebfd3e8fa1a997fc41f0f11
mrios <riosmatias.zarate@gmail.com>
2020-06-04 13:05:35 -0300

    Refactor Basic Form UI, add collapse panels for files

--- src/components/projects/ProjectFormBasic.tsx
+++ src/components/projects/ProjectFormBasic.tsx
@@ -1,7 +1,19 @@
-import React, { FC, useState } from 'react';
-import { Form, Col, Row, Input, DatePicker, Upload, Button } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import React, { FC, useState, useEffect } from 'react';
+import {
+  Form,
+  Col,
+  Row,
+  Input,
+  DatePicker,
+  Upload,
+  Button,
+  Divider,
+  Collapse,
+} from 'antd';
+import { CaretRightOutlined, UploadOutlined } from '@ant-design/icons';
 import useProject from '../../hooks/useProject';
+
+const { Panel } = Collapse;
 
 const ProjectFormBasic: FC = (props) => {
   const [project] = useProject();
@@ -15,110 +27,150 @@ const ProjectFormBasic: FC = (props) => {
         'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
     },
   ]);
-  form.setFieldsValue(project);
+
+  useEffect(() => {
+    form.setFieldsValue(project);
+  }, []);
+
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
   return (
-    <Form layout="vertical" form={form} hideRequiredMark>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Form.Item
-            name="name"
-            label="Nombre"
-            rules={[
-              { required: true, message: 'Por favor, ingrese un nombre' },
-            ]}
-          >
-            <Input placeholder="Por favor, ingrese un nombre" />
-          </Form.Item>
+    <Form layout="vertical" form={form} hideRequiredMark onFinish={onFinish}>
+      <Row>
+        <Col span={13}>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="name"
+                label="Nombre"
+                rules={[
+                  { required: true, message: 'Por favor, ingrese un nombre' },
+                ]}
+              >
+                <Input allowClear placeholder="Por favor, ingrese un nombre" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="dateTime"
+                label="Fechas de inicio y fin"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Por favor, seleccione un rango de fechas',
+                  },
+                ]}
+              >
+                <DatePicker.RangePicker style={{ width: '100%' }} />
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Form.Item
+                name="description"
+                label="Descripcion"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Por favor, ingrese una descripcion',
+                  },
+                ]}
+              >
+                <Input.TextArea
+                  allowClear
+                  rows={4}
+                  placeholder="Ingrese una breve descripcion acerca del proyecto"
+                />
+              </Form.Item>
+            </Col>
+          </Row>
         </Col>
-        <Col span={12}>
-          <Form.Item
-            name="dateTime"
-            label="Fechas de inicio y fin"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, seleccione un rango de fechas',
-              },
-            ]}
+        <Col span={10} offset={1}>
+          <Collapse
+            bordered={false}
+            defaultActiveKey={['1']}
+            expandIcon={({ isActive }) => (
+              <CaretRightOutlined rotate={isActive ? 90 : 0} />
+            )}
+            className="custom-collapse"
           >
-            <DatePicker.RangePicker style={{ width: '100%' }} />
-          </Form.Item>
+            <Panel header="Archivos Guion" key="1" className="custom-panel">
+              <Row>
+                <Col span={12}>
+                  <Form.Item
+                    name="screenplay"
+                    label="Guion narrativo"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, suba el guion narrativo',
+                      },
+                    ]}
+                  >
+                    <Upload>
+                      <Button type="primary" ghost>
+                        <UploadOutlined /> Subir archivo
+                      </Button>
+                    </Upload>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    name="shootingScript"
+                    label="Guion tecnico"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, suba el guion tecnico',
+                      },
+                    ]}
+                  >
+                    <Upload>
+                      <Button type="primary" ghost>
+                        <UploadOutlined /> Subir archivo
+                      </Button>
+                    </Upload>
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Panel>
+            <Panel header="Otras Imagenes" key="2" className="custom-panel">
+              <Col span={24}>
+                <Form.Item
+                  name="images"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Por favor, suba el guion narrativo',
+                    },
+                  ]}
+                >
+                  <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                  >
+                    {fileList.length < 5 && '+ Upload'}
+                  </Upload>
+                </Form.Item>
+              </Col>
+            </Panel>
+          </Collapse>
         </Col>
       </Row>
-      <Row gutter={16}></Row>
-      <Row gutter={16}>
-        <Col span={24}>
-          <Form.Item
-            name="description"
-            label="Descripcion"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, ingrese una descripcion',
-              },
-            ]}
-          >
-            <Input.TextArea
-              rows={4}
-              placeholder="Ingrese una breve descripcion acerca del proyecto"
-            />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={6}>
-          <Form.Item
-            name="screenplay"
-            label="Guion narrativo"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, suba el guion narrativo',
-              },
-            ]}
-          >
-            <Upload>
-              <Button type="primary" ghost>
-                <UploadOutlined /> Subir archivo
-              </Button>
-            </Upload>
-          </Form.Item>
-        </Col>
-        <Col span={6}>
-          <Form.Item
-            name="shootingScript"
-            label="Guion tecnico"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, suba el guion tecnico',
-              },
-            ]}
-          >
-            <Upload>
-              <Button type="primary" ghost>
-                <UploadOutlined /> Subir archivo
-              </Button>
-            </Upload>
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="images"
-            label="Imagenes"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor, suba el guion narrativo',
-              },
-            ]}
-          >
-            <Upload
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              listType="picture-card"
-            >
-              {fileList.length < 5 && '+ Upload'}
-            </Upload>
+      <Row justify="end">
+        <Divider />
+        <Col>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Guardar
+            </Button>
+            <Button htmlType="button" onClick={onReset}>
+              Limpiar
+            </Button>
           </Form.Item>
         </Col>
       </Row>
