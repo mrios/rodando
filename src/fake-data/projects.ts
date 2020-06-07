a70b53d096e29e3943101792560759453512ed0c
mrios <riosmatias.zarate@gmail.com>
2020-06-07 13:49:26 -0300

    Update fake-data

--- src/fake-data/projects.ts
+++ src/fake-data/projects.ts
@@ -1,6 +1,6 @@
 export default [
   {
-    uid: '1',
+    uid: '71e3d3a0-a25b-48a4-9536-35ec0f420ec9',
     name: 'La Guardia',
     description: 'La Guardia es un proyecto realizado para Guion 3, FADU. UBA',
     dateTime: {},
@@ -16,7 +16,7 @@ export default [
     images: [],
   },
   {
-    uid: '2',
+    uid: '3b037121-e5b9-4d26-91c7-4146c48321be',
     name: 'Doña Clara (Aquarius)',
     description:
       'Clara, una ex-crítica musical de Recife de 65 años, vive retirada en un edificio particular, el Aquarius, construido en la década de 1940 sobre la chic Avenida Boa Viagem, que bordea el océano. Un importante promotor ha comprado todos los apartamentos, pero ella se niega a vender el suyo y emprende una guerra fría contra la empresa que la acosa. La estresante situación le perturba y le lleva a pensar en su vida, en su pasado, en sus seres queridos.',
@@ -33,7 +33,7 @@ export default [
     images: [],
   },
   {
-    uid: '3',
+    uid: '18b93a7d-2db5-434f-b033-fb6045e1f8bf',
     name: 'The Beastie boys story',
     description:
       'Mike Diamond y Adam Horovitz, dos de los miembros de Beastie Boys, ofrecen su visión íntima y personal sobre la historia de su banda y de más de 40 años de amistad. ',
