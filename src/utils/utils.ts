b5e6a7f0af384ce2531542187c569e22a59587e5
mrios <riosmatias.zarate@gmail.com>
2020-06-07 23:40:46 -0300

    Remove deprecated TODO

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,5 @@
 import { ProjectType } from '../state-containers/projects/ProjectTypes';
 
-// TODO: improve this function to recibe a prefix and in
 export const getUUIDFromURL = (pathname: string): string => {
   const pattern = `^/([a-zA-Z0-9_-]+)/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})`;
   var matched = new RegExp(pattern, 'g').exec(pathname);
